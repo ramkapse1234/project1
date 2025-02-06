4ca77c1605f095a0d6f21f188fabd658d1c9235a
ramkapse <parasuramrajoli@gmail.com>
2025-02-06 13:08:09 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to My First Netlify-Deployed React App 🚀</h1>
       <p>This app is now live on Netlify!</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ8liosYPhKGoWvLoiQbFr40EmGT1LjIzwOg&s" alt="avatar"/>
     </div>
   );
 }
