fd7f7088005dad5a32c9e4d0539c3c6737d85349
ramkapse <parasuramrajoli@gmail.com>
2025-02-06 12:41:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My First Netlify-Deployed React App 🚀</h1>
+      <p>This app is now live on Netlify!</p>
+    </div>
+  );
+}
+
+export default App;
