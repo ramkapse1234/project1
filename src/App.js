c8ccebbbb9b8faa24656f89429fd0a36c00e37c6
ramkapse <parasuramrajoli@gmail.com>
2025-02-06 13:32:44 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My First Netlify-Deployed React App 🚀</h1>
       <p>This app is now live on Netlify!</p>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ8liosYPhKGoWvLoiQbFr40EmGT1LjIzwOg&s" alt="avatar"/>
+      <h2>Para</h2>
     </div>
   );
 }
